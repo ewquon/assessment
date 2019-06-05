1bf69ba0f6c78ed908928c2336a1c693d423afaa
Eliot Quon <eliot.quon@nrel.gov>
2019-06-04 20:49:40 -0600

    Generalize inflow directory for turbsim setup

--- turbine.py
+++ turbine.py
@@ -40,12 +40,12 @@ class OpenFAST(object):
         tmp.generate(inputfile,replace=tmp_inputs)
         return tmp
 
-    def setup_turbsim(self,inputs={}):
+    def setup_turbsim(self,inputs={},inflowdir='Wind'):
         """Setup turbsim input files for all seeds"""
-        templatefile = os.path.join(self.cwd,'Wind','start.inp')
+        templatefile = os.path.join(self.cwd,inflowdir,'start.inp')
         for iseed, seed in enumerate(self.seeds):
             inputs['RandSeed1'] = seed
-            inputfile = os.path.join(self.cwd,'Wind','turbsim_{:02d}.inp'.format(iseed))
+            inputfile = os.path.join(self.cwd,inflowdir,'turbsim_{:02d}.inp'.format(iseed))
             if self.verbose:
                 print('Generating',inputfile,'from',templatefile)
             self._generate_input(inputfile,templatefile,inputs)
